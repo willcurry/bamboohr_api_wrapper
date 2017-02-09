7f7a2c34ad36d1d50e1cb544754b93e279bb91cf
Will Curry <watercrafthd@gmail.com>
2017-02-09 16:46:48 +0000

    Add group benefits, benefit coverage, whos out, specific employee methods

--- lib/bamboo_hr.rb
+++ lib/bamboo_hr.rb
@@ -18,8 +18,24 @@ class BambooHR
     data["employees"]
   end
 
+  def employee(id)
+    JSON.parse(employee_data(id).body)
+  end
+
   def time_off_requests
     JSON.parse(time_off_data.body)
+  end
+
+  def whos_out
+    JSON.parse(whos_out_data.body)
+  end
+
+  def benefit_coverage
+    JSON.parse(benefit_coverage_data.body)
+  end
+
+  def benefit_groups
+    JSON.parse(benefit_groups_data.body)
   end
 
   private
@@ -28,8 +44,24 @@ class BambooHR
     self.class.get("/api/gateway.php/#{@subdomain}/v1/employees/directory", basic_auth: auth_info, headers: headers)
   end
 
+  def benefit_groups_data
+    self.class.get("/api/gateway.php/#{@subdomain}/v1/benefitgroups", basic_auth: auth_info, headers: headers)
+  end
+
+  def benefit_coverage_data
+    self.class.get("/api/gateway.php/#{@subdomain}/v1/benefitcoverages", basic_auth: auth_info, headers: headers)
+  end
+
+  def employee_data(id)
+    self.class.get("/api/gateway.php/#{@subdomain}/v1/employees/#{id}", basic_auth: auth_info, headers: headers)
+  end
+
   def time_off_data
     self.class.get("/api/gateway.php/#{@subdomain}/v1/time_off/requests", basic_auth: auth_info, headers: headers)
+  end
+
+  def whos_out_data
+    self.class.get("/api/gateway.php/#{@subdomain}/v1/time_off/whos_out", basic_auth: auth_info, headers: headers)
   end
 
   def headers
